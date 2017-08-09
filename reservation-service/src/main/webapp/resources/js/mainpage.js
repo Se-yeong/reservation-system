8ff9102ff3a33f6674dd1b4e5bce76f89f2b41d9
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-08-09 16:57:43 +0900

    [FB] 메인페이지 기능 구현

--- reservation-service/src/main/webapp/resources/js/mainpage.js
+++ reservation-service/src/main/webapp/resources/js/mainpage.js
@@ -4,56 +4,87 @@
 
 $(function() {
 	var categoryId = 0;
+	var current =0;
 
 	$(".tab_lst_min > .item:last > .anchor").addClass("last");
-
 	$(".event_tab_lst > .item").on("click", categoryClick);
-
+	$(window).on("scroll", scroll);
+	getData();
+	
 	function categoryClick() {
 		var $this = $(this);
 		categoryId = $this.data("category");
 		$(".anchor.active").removeClass("active");
 		$this.find(".anchor").addClass("active");
-
+		 getData();
+	}
+	
+	function getData() {
+		var start = 0, amount = 4;
+		getCount();
+		getProduct(start, amount, "html");
+		
+	}
+	
+	function getCount() {
 		var url = "api/product/count";
-		var productUrl = "api/product";
-		var cachedCount = [];
 		if (categoryId !== 0) {
 			url = url + "/" + categoryId;
-			productUrl = productUrl + "/category/" + categoryId;
-
 		}
-
 		$.ajax({
-			url : url
-		}).then(function(data) {
-			console.log(data);
-			$(".event_lst_txt > .pink").text(data + "개");
-		});
-
-		var start = 0, amount = 4;
+			url : url,
+			dataType : "json"
+		}).then(showCount);
+	}
+	
+	function getProduct(start, amount, method) {
+		
+		var url = "api/product";
+		if (categoryId !== 0) {
+			url = url + "/category/" + categoryId;
+		}
+		
+		$.ajax({
+			url : url + "?start=" + start + "&amount=" + amount,
+			dataType : "json"
+		}).then(showProduct.bind(this, method));
+	}
+	
+	function showCount(data) {
+		$(".event_lst_txt > .pink").text(data + "개");
+	}
+	
+	function showProduct(method, data) {
 		var source = $("#product-template").html();
 		var template = Handlebars.compile(source);
-
-		$.ajax({
-			url : productUrl + "?start=" + start + "&amount=" + amount
-		}).then(function(data) {
-			// handle bar
-			var left = [];
-			var right =[];
-			
-			for (var i = 0, max = data.length; i < max; ++i) {
-				if (i % 2) {
-					left.push(data[i]);
-				} else {
-					right.push(data[i]);
-				}
+		
+		var left = [];
+		var right =[];
+		
+		for (var i = 0, max = data.length; i < max; ++i) {
+			if (i % 2) {
+				left.push(data[i]);
+			} else {
+				right.push(data[i]);
 			}
-			console.log(left);
-			console.log(right);
-			$(".lst_event_box:first").html(template({item: left}));
-			$(".lst_event_box:last").html(template({item: right}));
-		});
+		}
+		current += data.length;
+		console.log(data);
+		$(".lst_event_box:first")[method](template( {item: left} ));
+		$(".lst_event_box:last")[method](template( {item: right} ));
 	}
+	
+
+	function scroll(event) {
+		var bottom = $("footer").offset().top;
+		var scrollY = window.scrollY;
+		var height = window.innerHeight;
+		
+		if( scrollY + height > bottom ) {
+			getProduct(current, 8, "append");
+		}
+		
+	}
+	
 
 });
